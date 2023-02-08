a98d9df77c4ce7a1e6bff58d2b30e4634319bdff
Kaium5724 <124491989+Kaium5724@users.noreply.github.com>
2023-02-08 20:18:49 +0600

    lab-work01

---
+++ Java-lab-01/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+        /*Box b1 = new Box();
+        b1.height = 5;
+        b1.width = 10;
+        b1.length = 2;
+        b1.calculateArea();*/
+      Employee e1=new Employee();
+        e1.name = "Kaium";
+        e1.salary = 15000;
+        e1.display();
+
+
+    }
+}
